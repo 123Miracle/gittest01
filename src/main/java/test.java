2e123d25da21a62f878798289b8663ce422b5b51
Lawliet <861832116@qq.com>
2022-07-11 23:49:07 +0800

    Initial commit

---
+++ src/main/java/test.java
@@ -0,0 +1,9 @@
+/**
+ * @author ：Lawliet
+ * @date ：Created in 2022/7/11 23:33
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
